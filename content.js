8813be2a60be0a0c07a65ff02b910fec793a2e8f
Thomas Hexton <tom.hexton@gmail.com>
2019-05-11 17:35:36 +1000

    added readme and begun feature list

--- content.js
+++ content.js
@@ -1,88 +1,88 @@
-$( document ).ready( function(){
+$( document ).ready( function() {
 
-  console.log("running now brah");
+	console.log( "running now brah" );
 
-    ////// Nick cage image swapper
-    let placeCage = false;  //on or off
+	////// Nick cage image swapper
+	let placeCage = false; //on or off
 
-    /////// Bill Murray image swapper
-    let fillMurray = false; //on or off
+	/////// Bill Murray image swapper
+	let fillMurray = true; //on or off
 
-    /////// Paragraph Background color
-    let paragraphBackground = true; // on or off
-    let paragraphBackgroundColor = "blue"
-    let paragraphTextColor = "red"
+	/////// Paragraph Background color
+	let paragraphBackground = false; // on or off
+	let paragraphBackgroundColor = "blue"
+	let paragraphTextColor = "red"
 
-    ////// Custom Heading Messages
-    let customHeadingOn = false; // on or off
-    let customHeadingText = "Testing 1-2-3"
+	////// Custom Heading Messages
+	let customHeadingOn = false; // on or off
+	let customHeadingText = "Testing 1-2-3"
 
-    ///// Makes the image freakout
-    let imageFreaker = false;
+	///// Makes the image freakout
+	let imageFreaker = false;
 
 
-    //URL of the API
-    const url = "https://atm-rails-burning-airlines.herokuapp.com/airplanes.json"
+	//URL of the API
+	const url = "https://atm-rails-burning-airlines.herokuapp.com/airplanes.json"
 
-    if (fillMurray) {
-      let images = document.getElementsByTagName('img');
-      for (let i = 0, l = images.length; i < l; i++) {
-        images[i].src = 'http://fillmurray.com/' + images[i].width + '/' + images[i].height;
-      }
-    }
+	if ( fillMurray ) {
+		let images = document.getElementsByTagName( 'img' );
+		for ( let i = 0, l = images.length; i < l; i++ ) {
+			images[ i ].src = 'https://fillmurray.com/' + images[ i ].width + '/' + images[ i ].height;
+		}
+	}
 
-    if (placeCage) {
-      let images = document.getElementsByTagName('img');
-      for (let i = 0, l = images.length; i < l; i++) {
-        images[i].src = 'http://placecage.com/' + images[i].width + '/' + images[i].height;
-      }
-    }
+	if ( placeCage ) {
+		let images = document.getElementsByTagName( 'img' );
+		for ( let i = 0, l = images.length; i < l; i++ ) {
+			images[ i ].src = 'https://placecage.com/' + images[ i ].width + '/' + images[ i ].height;
+		}
+	}
 
-    if (customHeadingOn) {
-      let paragraphs = document.getElementsByTagName('h1');
-      for (let i = 0, l = paragraphs.length; i < l; i++) {
-        paragraphs[i].innerText = "customHeadingOn"
-      }
-    }
+	if ( customHeadingOn ) {
+		let paragraphs = document.getElementsByTagName( 'h1' );
+		for ( let i = 0, l = paragraphs.length; i < l; i++ ) {
+			paragraphs[ i ].innerText = "customHeadingOn"
+		}
+	}
 
-    if (paragraphBackground) {
-      let para = document.getElementsByTagName('p');
-      for (let i = 0, l = para.length; i < l; i++) {
-      para[i].style.backgroundColor = paragraphBackgroundColor
-      para[i].style.color = paragraphTextColor
-      }
-    }
+	if ( paragraphBackground ) {
+		let para = document.getElementsByTagName( 'p' );
+		for ( let i = 0, l = para.length; i < l; i++ ) {
+			para[ i ].style.backgroundColor = paragraphBackgroundColor
+			para[ i ].style.color = paragraphTextColor
+		}
+	}
 
-        javascript: repeatCount = 0;
-    x1 = .1;
-    y1 = .05;
-    x2 = .25;
-    y2 = .24;
-    x3 = 1.6;
-    y3 = .24;
-    x4 = 300;
-    y4 = 200;
-    x5 = 300;
-    y5 = 200;
+	javascript: repeatCount = 0;
+	x1 = .1;
+	y1 = .05;
+	x2 = .25;
+	y2 = .24;
+	x3 = 1.6;
+	y3 = .24;
+	x4 = 300;
+	y4 = 200;
+	x5 = 300;
+	y5 = 200;
 
-    images = document.getElementsByTagName( "img" );
-    imagesLength = images.length;
+	images = document.getElementsByTagName( "img" );
+	imagesLength = images.length;
 
-    if (imageFreaker) {
-      function imageFreak() {
-          for ( i = 0; i - imagesLength; i++ ) {
-              imagesShift = images[ i ].style;
-              imagesShift.position = 'absolute';
-              imagesShift.left = ( Math.sin( repeatCount * x1 + i * x2 + x3 ) * x4 + x5 ) + "px";
-              imagesShift.top = ( Math.cos( repeatCount * y1 + i * y2 + y3 ) * y4 + y5 ) + "px"
-          }
-          repeatCount++
-      }
-      setInterval( 'imageFreak()', 5 );
-      void( 0 );
-    }
+	if ( imageFreaker ) {
+		function imageFreak() {
+			for ( i = 0; i - imagesLength; i++ ) {
+				imagesShift = images[ i ].style;
+				imagesShift.position = 'absolute';
+				imagesShift.left = ( Math.sin( repeatCount * x1 + i * x2 + x3 ) * x4 + x5 ) + "px";
+				imagesShift.top = ( Math.cos( repeatCount * y1 + i * y2 + y3 ) * y4 + y5 ) + "px"
+			}
+			repeatCount++
+		}
+		setInterval( 'imageFreak()', 5 );
+		void( 0 );
+	}
 
-});
+} );
 
 
 
--- js/background.js
+++ js/background.js
@@ -1,9 +1,21 @@
-console.log("background.js");
+console.log( "background.js" );
 
 
 const url = "https://atm-rails-burning-airlines.herokuapp.com/airplanes.json"
 
-  $.ajax( url )
-  .done(function(response){
-    console.log('API response',response[1].model);
-  });
+$.ajax( url )
+	.done( function( response ) {
+		console.log( 'jQuery response', response[ 1 ].model );
+	} );
+
+let results = {};
+fetch( url )
+	.then( function( response ) {
+		return response.json();
+	} )
+	.then( function( myJSON ) {
+		results = myJSON;
+	} )
+	.catch( function( error ) {
+		console.error( error );
+	});
