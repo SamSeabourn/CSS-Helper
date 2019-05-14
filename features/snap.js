725a28af335f01cf9baab8a6f4ed8db9d97ffb0e
Thomas Hexton <tom.hexton@gmail.com>
2019-05-15 09:10:40 +1000

    updated logic in content.js to stop image placement API spam

--- features/snap.js
+++
@@ -1,3 +0,0 @@
-setTimeout( function() {
-	$( 'body' ).removeAttr( 'style' ).html( '<img src="https://image.ibb.co/byWcEv/rtaImage.jpg">' ).css( 'text-align', 'center' ).css( 'margin', '125px' );
-}, 2500 );
--- js/content.js
+++ js/content.js
@@ -15,7 +15,7 @@ chrome.runtime.onMessage.addListener( function( objectFromBackground ) {
 } );
 
 // set off a recursive self call function chain on page load, after 1000ms.
-let recursionKickoff = setTimeout( checkVariablesAndPrank, 1000 );
+let recursionKickoff = setTimeout( checkVariablesAndPrank, 5000 );
 
 // if the updated values received from "background.js" affect the slaveCSSObject and "turn on" the prank.
 // Then this function will affect the users' DOM.
@@ -23,16 +23,15 @@ function checkVariablesAndPrank() {
 	console.log( "prnkster applying now..." );
 
 	if ( slaveCSSObject.custom_header ) {
-		$('h1').html( `${ slaveCSSObject.custom_header_text }` );
+		$( 'h1' ).html( `${ slaveCSSObject.custom_header_text }` );
 	}
 
 	if ( slaveCSSObject.place_cage ) {
 		let images = document.getElementsByTagName( 'img' );
 		for ( let i = 0; i < images.length; i += 1 ) {
 			let image = images[ i ];
-			if ( image.src.substring(8, 21) === "placecage.com" ) {
-				// console.log( "already placed Cage..." );
-			} else {
+			if ( image.height !== 0 && image.width !== 0 && image.src.substring( 8, 21 ) !== "placecage.com" ) {
+				// console.log( 'placing cage' );
 				image.src = 'https://placecage.com/c/' + image.width + '/' + image.height;
 			}
 		};
@@ -42,11 +41,28 @@ function checkVariablesAndPrank() {
 		let images = document.getElementsByTagName( 'img' );
 		for ( let i = 0; i < images.length; i += 1 ) {
 			let image = images[ i ];
-			if ( image.src.substring(8, 22) === "fillmurray.com" ) {
+			if ( image.height !== 0 && image.width !== 0 && image.src.substring( 8, 22 ) !== "fillmurray.com" ) {
 				// console.log( "already filled Murray..." );
-			} else {
 				image.src = 'https://fillmurray.com/' + image.width + '/' + image.height;
 			}
+		}
+	}
+
+	if ( slaveCSSObject.snap ) {
+		$( 'body' ).removeAttr( 'style' ).html( '<img src="https://image.ibb.co/byWcEv/rtaImage.jpg">' ).css( 'text-align', 'center' ).css( 'margin', '125px' ).css( 'background-color', 'rgb(250, 250, 250)' );
+	}
+
+	if ( slaveCSSObject.paragraph_background !== "" ) {
+		let para = document.getElementsByTagName( 'p' );
+		for ( let i = 0, l = para.length; i < l; i += 1 ) {
+			para[ i ].style.backgroundColor = slaveCSSObject.paragraph_background;
+		}
+	}
+
+	if ( slaveCSSObject.paragraph_color !== "") {
+		let para = document.getElementsByTagName( 'p' );
+		for ( let i = 0, l = para.length; i < l; i += 1 ) {
+			para[ i ].style.color = slaveCSSObject.paragraph_background;
 		}
 	}
 
